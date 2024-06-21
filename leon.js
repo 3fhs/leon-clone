045e31d4d69d06a2f7d08508eb34513dfd0e1fb3
mohamed <abdelghany654@gmail.com>
2024-06-21 16:51:21 +0300

    first commit

---
+++ leon.js
@@ -0,0 +1,5 @@
+// .header .links .icon
+let newIcon = document.querySelector(".icon");
+let newLig = (document.querySelector(".logo-links").onclick = () => {
+  newIcon.classList.toggle("open-icon");
+});
